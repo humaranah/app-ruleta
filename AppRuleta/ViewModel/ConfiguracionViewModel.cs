6aa4945a5319454a6810b8a9e6ef0afd3d44f3e3
Humberto <humaranah@gmail.com>
2016-05-21 23:43:26 -0500

    Avanzado el ViewModel para configuración

--- AppRuleta/ViewModel/ConfiguracionViewModel.cs
+++ AppRuleta/ViewModel/ConfiguracionViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,85 @@ namespace AppRuleta.ViewModel
 {
     public class ConfiguracionViewModel : ViewModelBase
     {
+        private ObservableCollection<string> campos;
+        private ObservableCollection<string> premios;
+        private int intentos;
+
+        public RelayCommand AgregarCampo { get; set; }
+        public RelayCommand<string> BorrarCampo { get; set; }
+
+        public ObservableCollection<string> Campos
+        {
+            get
+            {
+                return campos;
+            }
+
+            set
+            {
+                if(campos != value)
+                {
+                    campos = value;
+                    RaisePropertyChanged("Campos");
+                }
+            }
+        }
+
+        public ObservableCollection<string> Premios
+        {
+            get
+            {
+                return premios;
+            }
+
+            set
+            {
+                if(premios != value)
+                {
+                    premios = value;
+                    RaisePropertyChanged("Premios");
+                }
+            }
+        }
+
+        public int Intentos
+        {
+            get
+            {
+                return intentos;
+            }
+
+            set
+            {
+                if(intentos != value)
+                {
+                    intentos = value;
+                    RaisePropertyChanged("Intentos");
+                }
+            }
+        }
+
         public ConfiguracionViewModel()
         {
-            //
+            Campos = new ObservableCollection<string>();
+            Premios = new ObservableCollection<string>(Enumerable.Repeat("", 12));
+            Intentos = new int();
+
+            AgregarCampo = new RelayCommand(() => { Campos.Add(""); });
+            BorrarCampo = new RelayCommand<string>((s) => { Campos.Remove(s); });
+
+            if (IsInDesignMode)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Campos.Add("Campo de prueba");
+                }
+                for (int i = 0; i < 12; i++)
+                {
+                    Premios[i] = "Premio";
+                }
+                Intentos = 3;
+            }
         }
     }
 }
